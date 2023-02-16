845b9af60ef0c3ef8a03a111c455a97b6be57e03
shahriar <shahriarkabir078@gmail.com>
2023-02-16 14:31:10 +0600

    recurnsive funtion

---
+++ recursion sum of array.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n, int a[]){
+	if(n<0){
+		return 0;
+	}
+	else{
+
+	return a[n] + sum(n-1,a);
+ 
+	}
+
+}
+
+int main(){
+int n;
+cin>>n;
+
+int arr[n];
+for(int i = 0 ; i < n ; i++ ){
+	cin>>arr[i];
+	cout<<arr[i]<<" ";
+}
+
+cout<<endl<<sum(3,arr);
+
+}
+
+
+
