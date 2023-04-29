56aeb5229bba640891523fd40c00463c074d47f1
shahriar <shahriarkabir078@gmail.com>
2023-04-29 17:50:11 +0600

    new

---
+++ Practice/Vector/1.vector_basic.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<int>&v){
+for(int i = 0 ; i < v.size(); i++){
+
+    cout<<v[i]<<endl;
+
+    
+}
+
+}
+
+int main(){
+    vector<int>v;
+
+    v.push_back(5);
+    v.push_back(6);
+
+
+    printVec(v);
+}
+  
+
