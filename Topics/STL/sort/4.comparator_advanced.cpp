c0a932a69ab24e484c821c032639602daf8e9363
shahriar <shahriarkabir078@gmail.com>
2023-06-20 22:28:05 +0600

    new

---
+++ Topics/STL/sort/4.comparator_advanced.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int should_i_swap(pair<int,int> a , pair<int,int> b){
+	if(a > b) return true ;
+	return false;
+}
+
+int main(){
+	int n;
+	cin>>n;
+
+
+	vector<pair<int,int>> v(n);
+
+	for(int i = 0 ; i < n ; i++){
+		cin>>v[i].first>>v[i].second;
+	}
+	for(int i = 0 ; i < n ; i++){
+		for(int j = i+1 ; j < n ; j++){
+			if(should_i_swap(v[i],v[j]))
+			swap(v[i],v[j]);
+		}
+	}
+
+	for(auto i : v){
+		cout<<i.first << " "<<i.second<<endl;
+	}
+}
