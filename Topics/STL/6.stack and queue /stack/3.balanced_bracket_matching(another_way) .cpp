3ca6da8e1e2c1876a711d10d8384d227b0b5ae2f
shahriar <shahriarkabir078@gmail.com>
2023-06-23 16:19:16 +0600

    new

--- Topics/STL/6.stack and queue /stack/3.balanced_bracket_matching(another_way) .cpp
+++ Topics/STL/6.stack and queue /stack/3.balanced_bracket_matching(another_way) .cpp
@@ -7,14 +7,15 @@ unordered_map<char , int > m ={{'(',-1},{'{',-2},{'[',-3},{')',1},{'}',2},{']',3
 			if(m[bracket] < 0){
 				st.push(bracket);
 			}
+			
 			else {
 				if(st.empty()) return "NO";
 				char top = st.top();
 				st.pop();
-
 				if(m[top] + m[bracket]!=0) return "NO";
 			}
 		}
+
 		if(st.empty()) return "YES";
 		else return "NO";
 
