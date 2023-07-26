51179492a1fb1937c9af203c7bbe4fb5d6c35fd7
shahriar <shahriarkabir078@gmail.com>
2023-07-26 14:29:23 +0600

    new

--- Codeforces/Div 3/A/2.Escalator Conversation(888).cpp
+++ Codeforces/Div 3/A/2.Escalator Conversation(888).cpp
@@ -1,23 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_sorted(const vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void showBinary(int a){
-	for(int i =  5; i >= 0 ; --i){
-		cout<<((a>>i)&1);
-		}
-		cout<<endl;
-}
-
 
 void solve(){
 
