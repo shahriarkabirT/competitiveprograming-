e070f05ab92acf4b560ef90a6ce62d479c4efc6d
shahriar <shahriarkabir078@gmail.com>
2024-03-13 21:09:09 +0600

    new

--- Topics/Array/7.1 Kadane's Algorithm problems.cpp
+++ Topics/Array/7.1 Kadane's Algorithm problems.cpp
@@ -1,31 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef bool                      boo;
-typedef int                       li;
-typedef long long int             ll;
-typedef unsigned long long int    lu;
-typedef double                    db;
-
-
 //--------------------------------------------------------------------------//
 
 
-#define out 			cout<<
+
 #define loop 			for(int i = 0 ; i < n; i++)
-#define vecint			vector<int>
-#define sortasc			sort(a.begin(),a.end())
-#define sortdsc			sort(a.begin(),a.end(),greater<int>())		
-#define revloop 		for(int i = n-1; i>=0 ; i--)
-#define tc              int t; cin>>t; while(t--)
-#define faster          ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-#define nl              <<endl
-#define in 				cin>>
-#define gcd(a,b)        __gcd(a,b)
-#define in_out          freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define spc 			<<" "<<
-//#define int long long
-//--------------------------------------------------------------------------//
 
 
 string maxSum(string w,char x[], int b[],int n){
@@ -85,7 +65,7 @@ string maxSum(string w,char x[], int b[],int n){
       }
 signed main(){
         //in_out
-        faster
+
 
         string w;
         int n;
