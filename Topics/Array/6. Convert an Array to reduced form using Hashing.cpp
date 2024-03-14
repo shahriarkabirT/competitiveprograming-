626a0e33d71fe47c3b56ffd43050b0a6cdf44952
shahriar <shahriarkabir078@gmail.com>
2024-03-14 23:45:37 +0600

    new

--- Topics/Array/6. Convert an Array to reduced form using Hashing.cpp
+++ Topics/Array/6. Convert an Array to reduced form using Hashing.cpp
@@ -1,31 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef bool                      boo;
-typedef int                       li;
-typedef long long int             ll;
-typedef unsigned long long int    lu;
-typedef double                    db;
-
-
-//--------------------------------------------------------------------------//
-
-
-#define out 			cout<<
-#define loop 			for(int i = 0 ; i < n; i++)
-#define vecint			vector<int>
-#define revloop 		for(int i = n-1; i>=0 ; i--)
-#define tc              int t; cin>>t; while(t--)
-#define faster          ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-#define nl              <<endl
-#define in 				cin>>
-#define gcd(a,b)        __gcd(a,b)
-#define in_out          freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define spc 			<<" "<<
-//#define int long long
-//--------------------------------------------------------------------------//
-
-
 
 void convert(int arr[], int n)
 {
